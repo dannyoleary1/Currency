1faa99a4b732939b1413829faa13aebe818f5612
admin <dannyoleary1@gmail.com>
2018-03-16 08:58:28 +0000

    Added and removed comments

--- Currency/ViewController.swift
+++ Currency/ViewController.swift
@@ -13,7 +13,7 @@ import ActionSheetPicker_3_0
 
 class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource{
     
-    
+    //this is our tableView where all our currencies will be stored.
     @IBOutlet weak var tableView: UITableView!
     var currencyDict:Dictionary = [String:CurrencyObject]()
     var baseCurrency:Currency = Currency.init(name:"EUR", rate:1, flag:"🇪🇺", symbol:"€")!
@@ -33,9 +33,8 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // print("currencyDict has \(self.currencyDict.count) entries")
-
+        
+        //Create a list of static currencies and then display them as part of the table view
         createCurrencyList()
         tableView.reloadData()
         
@@ -49,22 +48,20 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
         baseFlag.text = baseCurrency.flag
         
         // set up last updated date
-        
         dateformatter.dateFormat = "dd/MM/yyyy hh:mm a"
         lastUpdatedDateLabel.text = dateformatter.string(from: lastUpdatedDate)
-        
-        
         
         baseTextField.delegate = self
         baseTextField.addDoneButtonToKeyboard(myAction: #selector(self.baseTextField.resignFirstResponder))
 
-        //TODO eference and fiure out
         self.convert(self)
     }
     
+    /*****************************************************************************
+    // This function is responsible for adding the currencies to a dictionary.
+    // The dictionary will be used to populate the tableView.
+    ******************************************************************************/
     func createCurrencyList(){
-        //let c:Currency = Currency(name: name, rate: rate!, flag: flag, symbol: symbol)!
-        //self.currencyDict[name] = c
         currencyDict["GBP"] = CurrencyObject(fullName: "Great British Pound", symbol: "£", value: "1.00", flag: "🇬🇧")
         currencyDict["USD"] = CurrencyObject(fullName: "United States Dollar", symbol: "$",
                                           value: "1.00", flag: "🇺🇸")
@@ -80,38 +77,37 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
         // Dispose of any resources that can be recreated.
     }
     
+    /*****************************************************************************
+     // This function is an asynchronus call to the api which will get back the latest
+     // results in a JSON format. It includes a plugin called 'NSActivityIndicatorView'.
+     // https://github.com/ninjaprox/NVActivityIndicatorView
+     ******************************************************************************/
     func getConversionTable(){
         let urlStr:String = "https://api.fixer.io/latest"
         var request = URLRequest(url: URL(string: urlStr)!)
         request.httpMethod = "GET"
         
+        //setup a frame for activity indicator and then set it to a type, and color.
         let x = self.view.frame.width / 2
         let y = self.view.frame.height / 2
         let frame = CGRect(x: (x - 40), y: (y - 50), width: 45, height: 45)
         var activityIndicator = NVActivityIndicatorView(frame: frame)
-        activityIndicator.type = . ballScale // add your type
-        activityIndicator.color = UIColor.red // add your color
+        activityIndicator.type = . ballScale
+        activityIndicator.color = UIColor.red
         
-        self.view.addSubview(activityIndicator) // or use  webView.addSubview(activityIndicator)
+        self.view.addSubview(activityIndicator)
         activityIndicator.startAnimating()
         
         let session = URLSession.shared
         let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
         
             if error == nil{
-                //print(response!)
                 do {
                     let jsonDict = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String:Any]
-                    // print(jsonDict)
-                    
                     if let ratesData = jsonDict["rates"] as? NSDictionary {
-                        //print(ratesData)
                         for rate in ratesData{
-                            //print("#####")
                             let name = String(describing: rate.key)
                             let rate = (rate.value as? NSNumber)?.doubleValue
-                            //var symbol:String
-                            //var flag:String
                             if let currentCurrency = self.currencyDict[name]{
                                 currentCurrency.value = String(format: "%.02f", rate!)
                                 self.currencyDict[name] = currentCurrency
@@ -137,21 +133,34 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
             }
         })
         task.resume()
-        
     }
     
+    /*****************************************************************************
+     // This function is responsible for passing the convert function from the button.
+     // It passes to the convertCurrencies() where it will update the table, and rates.
+     ******************************************************************************/
     @IBAction func convert(_ sender: Any) {
         //TODO check if this is meant to make a new call.
         //getConversionTable()
        convertCurrencies()
     }
     
+    /*****************************************************************************
+     // This function is responsible for refreshing the current rate. It first updates
+     // the newer rates, and then it converts them which will update the rates in the
+     // table view.
+     ******************************************************************************/
     @IBAction func refresh(_ sender: Any){
         //TODO Is this supposed to refresh to 1.0?
         getConversionTable()
         convertCurrencies()
     }
     
+    /*****************************************************************************
+     // Function responsible for converting the currencys by the base currency.
+     // It changes the values to represent this in the currencyDict, and then
+     // reloads the tableView.
+     ******************************************************************************/
     func convertCurrencies(){
         var result = 0.0
         if let euro = Double(baseTextField.text!) {
@@ -164,22 +173,35 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
             }
         }
         tableView.reloadData()
-        lastUpdatedDate = Date()
-        lastUpdatedDateLabel.text = dateformatter.string(from: lastUpdatedDate)
+        //TODO I do not think these are needed. Make sure
+       // lastUpdatedDate = Date()
+       // lastUpdatedDateLabel.text = dateformatter.string(from: lastUpdatedDate)
     }
     
+    /*****************************************************************************
+     // Function for setting how many Sections we want in our tableView. In our case
+     // it will always be one.
+     ******************************************************************************/
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
+    /*****************************************************************************
+     // Function that sets up the number of cells/ rows in the table. It will be the
+     // ammount of currencies that we have in the system.
+     ******************************************************************************/
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return currencyDict.count
     }
     
+    /*****************************************************************************
+     // Function that iterates through every cell and assigns the corresponding values
+     // from the array to the entry in the cell.
+     ******************************************************************************/
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CurrencyTableView
         
-        //getting the hero for the specified position
+        //getting the currency for the specified position
         let currentCur: CurrencyObject
         currentCur = Array(currencyDict.values)[indexPath.row]
         
@@ -190,7 +212,13 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
         return cell
     }
     
-    
+    /*****************************************************************************
+     // Function that deals with the add button. It displays a list of all the other
+     // currencies that are not static in the system. It displays a PickerView using
+     // a plugin called 'ActionSheetPicker_3_0'. When picked, it wil add the currency
+     // to the currencyDict dictionary and update the table to represent the change.
+     // https://github.com/skywinder/ActionSheetPicker-3.0
+     ******************************************************************************/
     @IBAction func buttonAdd(_ sender: Any) {
         var allC = AllCurrencies.additionalCurrencies()
         var names = [String]()
@@ -210,17 +238,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
                     self.getConversionTable()
                     self.tableView.reloadData()
                 }
-                
-                
                 return
         }, cancel: { ActionMultipleStringCancelBlock in return }, origin: sender)
     }
-    
-    
-    /*
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     */
 }
-
